cff0646b838bb217f2df293ef34caf74a40a962f
bardend <ricardo.ulloa.v@uni.pe>
2025-10-21 21:18:16 -0500

    Deploy a base of spark with hdfs hadoop, with the templates in config/

--- base/config-hadoop.sh
+++ base/config-hadoop.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 # Configurations of .xml on /etc/hadoop for dfs,yarn,map,...
+# Configurations of spark /spark/config/ for master, worker
 set -e
-TEMPLATE_DIR="/opt/hadoop-templates"
-HADOOP_XML_DIR="/etc/hadoop"
+TEMPLATE_DIRS=(
+    "${SPARK_TEMPLATE_TMP}"
+    "${HADOOP_TEMPLATE_TMP}"
+)
+
+OUTPUT_DIRS=(
+    "${SPARK_CONFIG_DIR}"
+    "${HADOOP_CONFIG_DIR}"
+)
 
 process_template() {
     local template_file=$1
     local output_file=$2
-
-    if [[ -f "$template_file" ]]; then
-        perl -pe '
-            s/\$\{(\w+):-([^}]+)\}/ 
-                my $var = $1;
-                my $default = $2;
-                exists $ENV{$var} ? $ENV{$var} : $default;
-            /gex;
-        ' "$template_file" > "$output_file"
-    fi
-
+    perl -pe '
+        s/\$\{(\w+):-([^}]+)\}/ 
+            my $var = $1;
+            my $default = $2;
+            exists $ENV{$var} ? $ENV{$var} : $default;
+        /gex;
+    ' "$template_file" > "$output_file"
 }
-
-for template in "$TEMPLATE_DIR"/*.template; do
-    file=$(basename "$template")
-    file_name="${file%.*}"
-    process_template "$template" "$HADOOP_XML_DIR/$file_name"
+# Iterar por cada par de directorios
+for i in "${!TEMPLATE_DIRS[@]}"; do
+    TEMPLATE_DIR="${TEMPLATE_DIRS[$i]}"
+    OUTPUT_DIR="${OUTPUT_DIRS[$i]}"
+    
+    echo "Procesing : ${TEMPLATE_DIR} -> ${OUTPUT_DIR}"
+    
+    # Procesar cada template en el directorio actual
+    for template in "$TEMPLATE_DIR"/*.template; do
+        file=$(basename "$template")
+        file_name="${file%.*}"
+        process_template "$template" "$OUTPUT_DIR/$file_name"
+        echo "Procesing : ${file} -> {$OUTPUT_DIR/$file_name}"
+    done
+    echo "Final array"
 done
 
+echo "Final Deberia /bin/bash"
 exec "$@"
